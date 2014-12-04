4d05225fb7a84ada4fed1fa93d18593f157018d3
Michael Barz <mb@barz.de>
2014-12-04 14:37:05 +0100

    updated lib path

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.extension import Extension
 
 from Cython.Build import cythonize
 
-fromSource = False
+fromSource = True
 
 extensions = None
 if (fromSource):
@@ -12,7 +12,7 @@ if (fromSource):
                     language="c++",
                     include_dirs= ['videoInput/videoInputSrcAndDemos/libs/'],
                     library_dirs= ['videoInput/videoInputSrcAndDemos/libs/DShow/lib/', 'videoInput/videoInputSrcAndDemos/libs/glfw', 'videoInput/videoInputSrcAndDemos/libs/extra/'],
-                    libraries= ['ddraw', 'dxguid', 'ole32', 'strmiids', 'uuid', 'glfw'],
+                    libraries= ['ddraw', 'dxguid', 'ole32', 'oleaut32', 'strmiids', 'uuid', 'glfw'],
                     extra_compile_args=['/EHsc', '/Zc:wchar_t'],
                     extra_link_args=['/MACHINE:X64', '/NOLOGO', '/LTCG'])  
     ]
@@ -20,9 +20,10 @@ else:
     extensions = [Extension(  name="pyVideoInput",
                 sources=['pyVideoInput.pyx'],
                 language="c++",
-                extra_objects= ['videoInput.lib'],
+                extra_objects= ['videoInput.lib', 'oleaut32.lib'],
                 include_dirs= ['videoInput/videoInputSrcAndDemos/libs/'],
-                library_dirs= ['videoInput/compiledLib/vs2012/x64/'])
+                library_dirs= ['vi_x64/', 'videoInput/videoInputSrcAndDemos/libs/DShow/lib/'],
+                extra_link_args=['/MACHINE:X64', '/NODEFAULTLIB:LIBCMT'])
     ]
 
 setup(  name="pyVideoInput",
