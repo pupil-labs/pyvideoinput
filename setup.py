bf26c4838a35522f7c312ac7b7a0edd175ce1dd2
Michael Barz <mb@barz.de>
2015-02-13 10:18:47 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+data = [('', ['videoInput/x64/Release/_videoInput.lib', 'videoInput/x64/Release/_videoInput.pyd'])]
+
+setup(name='pyvideoinput',
+      version='0.4',
+      py_modules = ['videoInput'],
+      data_files=data
+      )
